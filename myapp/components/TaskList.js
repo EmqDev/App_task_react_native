eccefb32ccc71e18a306277449aca9eac467417d
Estebamq <quiroz_estebanmarcelo@live.com>
2024-04-04 03:23:05 -0300

    change page homeScreen reload task

--- myapp/components/TaskList.js
+++ myapp/components/TaskList.js
@@ -1,20 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { getTask } from '../api';
 import { Text, View, FlatList } from 'react-native'
 import TaskItem from './TaskItem'
+import { RefreshControl } from 'react-native-gesture-handler';
 
-const TaskList = ({tasks})=> {
+const TaskList = () => {
 
-  const renderItem = ({item}) =>{
-    return <TaskItem task={item}/>;
+  const [tasks, setTasks] = useState([]);
+  const [refreshing, setRefreshing] = useState(false);
+
+  const loadTask = async () => {
+    const data = await getTask();
+    setTasks(data);
+
+  }
+
+  useEffect(() => {
+    loadTask()
+  }, [])
+
+  const renderItem = ({ item }) => {
+    return <TaskItem task={item} />;
   };
-    
+
+  const onRefresh = React.useCallback(async () => {
+    setRefreshing(true);
+    await loadTask();
+    setRefreshing(false);
+  });
+
   return (
-      <FlatList
-      style={{width:"100%"}} 
-          data={tasks}
-          keyExtractor={(item) => item.id + ''}
-          renderItem={renderItem}
+    <FlatList
+      style={{ width: "100%" }}
+      data={tasks}
+      keyExtractor={(item) => item.id + ''}
+      renderItem={renderItem}
+      refreshControl={
+        <RefreshControl
+          refreshing={refreshing}
+          colors={["#78e08f"]}
+          progressBackgroundColor="#0a3d62"
+          onRefresh={onRefresh}
         />
+      }
+    />
   );
 };
 
--- myapp/screens/HomeScreen.js
+++ myapp/screens/HomeScreen.js
@@ -1,34 +1,15 @@
-import React, {useEffect, useState} from 'react'
-import { getTask } from '../api';
+import React from 'react'
 import TaskList from '../components/TaskList';
 import Layout from '../components/Layout';
 
 
 
-const HomeScreen = () => {
+const HomeScreen = () => (
+  <Layout>
+    <TaskList />
+  </Layout>
 
-  const [tasks, setTasks] = useState([]);
-
-  const loadTask = async () =>{
-    const data = await getTask();
-    setTasks(data);
-  
-  }
-
-  useEffect(()=>{
-    loadTask()
-  },[])
-
-
-    return (
-      <Layout>
-        <TaskList tasks={tasks}/>
-      </Layout>
-            
-    );
-}
-
-
+);
 
 export default HomeScreen;
 
